707b10f1d370fa04f6bb571261f2f4605f54434d
Jos-EDF <62007099+Jos-EDF@users.noreply.github.com>
2020-04-17 15:15:39 +0200

    Test pene

--- GothicCharacters/src/com/loncha/gothiccharacters/Main.java
+++ GothicCharacters/src/com/loncha/gothiccharacters/Main.java
@@ -34,6 +34,7 @@ public class Main extends JavaPlugin implements Listener{
 		cCajas = new CargarCajas(this);
 		rAleatoria = new RopaAleatoria();
 		
+		//Pene
 		//Registrar los listeners de cada una de las clases
 		getServer().getPluginManager().registerEvents(this, this);
 		getServer().getPluginManager().registerEvents(this.iCustom, this);
